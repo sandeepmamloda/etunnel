b5be6ddbbc590fa12a58169a7af83310238d4d3d
Sandeep <sandeep.mamloda@vselindia.com>
2026-04-16 16:26:14 +0530

    changes

---
+++ src/app/coretech/fingervein/page.jsx
@@ -0,0 +1,10 @@
+import Herotech from "@/components/coretech/p2n2/herotech-p2n2/herotech";
+const Fingervein = () => {
+    return (
+        <>
+            <Herotech />
+        </>
+    );
+};
+
+export default Fingervein;
